50640f17dc4fbe17d865ffb8429f28d1d6b7758d
Administrator <Administrator@60.207.84.122>
2018-07-28 15:52:03 +0800

    第十九次提交，暂存

--- hello/Main.java
+++ hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");//第十八次提交，暂存
+		System.out.println("Hello World!");//第十九次提交，暂存
 	}
 
 }
