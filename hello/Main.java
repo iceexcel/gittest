706faee0cbce7bb318002e4c4a7b89c466f39845
Administrator <Administrator@60.207.84.122>
2018-07-28 15:33:52 +0800

    第十四次提交，主干提交

--- hello/Main.java
+++ hello/Main.java
@@ -1,10 +1,9 @@
 package hello;
 
 public class Main {
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");//第十一次提交，删除无用的代码
+		System.out.println("Hello World!");//第十四次提交，主干提交
 	}
 
 }
