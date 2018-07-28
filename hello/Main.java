4fedbb172b542b57bf676b6a7b0ec708acfc4619
Administrator <Administrator@60.207.84.122>
2018-07-28 15:08:45 +0800

    主干第四次提交，冲突了

--- hello/Main.java
+++ hello/Main.java
@@ -2,8 +2,13 @@ package hello;
 
 public class Main {
 
+<<<<<<< Upstream, based on 3e7252b87204f695613509cc08382a6942fd5b33
 	public static void main(String[] args) {
-		System.out.println("Hello World!");//主干第四次提交
+		System.out.println("Hello World!");//主干第四次提交，回到第四次提交，实际七
+=======
+	public static void main(String[] args) {//123
+		System.out.println("Hello World!");//主干第六次提交
+>>>>>>> ce5b2ac six
 	}
 
 }
