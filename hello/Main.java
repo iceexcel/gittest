ce5b2acd77d623a8a1d7eadb96e76d11034a36e1
Administrator <Administrator@60.207.84.122>
2018-07-28 15:15:10 +0800

    six

--- hello/Main.java
+++ hello/Main.java
@@ -2,8 +2,8 @@ package hello;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");//主干第五次提交
+	public static void main(String[] args) {//123
+		System.out.println("Hello World!");//主干第六次提交
 	}
 
 }
