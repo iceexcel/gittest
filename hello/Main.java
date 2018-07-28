5f0ca8c67ff6ef234edee3fc228e521e36687576
Administrator <Administrator@60.207.84.122>
2018-07-28 15:28:32 +0800

    第十一次提交，删除无用的代码

--- hello/Main.java
+++ hello/Main.java
@@ -4,12 +4,7 @@ public class Main {
 <<<<<<< HEAD
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");//第九次提交
-=======
-
-	public static void main(String[] args) {//123
-		System.out.println("Hello World!");//主干第六次提交
->>>>>>> branch 'master' of https://github.com/iceexcel/gittest.git
+		System.out.println("Hello World!");//第十一次提交，删除无用的代码
 	}
 
 }
