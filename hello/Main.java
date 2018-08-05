4770e2b495b55c3345202d45215caff19f4bbcc9
Administrator <Administrator@A038>
2018-08-05 11:04:17 +0800

    第三次合并代码

--- hello/Main.java
+++ hello/Main.java
@@ -3,10 +3,14 @@ package hello;
 public class Main {
 
 	public static void main(String[] args) {
+<<<<<<< Upstream, based on 718fa937cfa5151127a6e5f68459aa47888d2cbd
 <<<<<<< HEAD
 		System.out.println("Hello World!");//第二十六次提交
 =======
 		System.out.println("Hello World!");//第二十四次提交 第二次合并代码
+=======
+		System.out.println("Hello World!");//第二十七次提交
+>>>>>>> 0b4e3a6 第二十七次提交
 	}
 
 }
