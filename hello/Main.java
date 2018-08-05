86868d5b7e8fb925fcf61f63016662b424df7d8b
Administrator <Administrator@A038>
2018-08-05 12:07:26 +0800

    dev分支，三十四次提交

--- hello/Main.java
+++ hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");//第十三次提交，删除无用的代码dev003
+		System.out.println("Hello World!");//dev分支，三十四次提交
 	}
 
 }
