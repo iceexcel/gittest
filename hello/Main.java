4f20e9972112541fc7ccfc949913feee6d978e62
Administrator <Administrator@A038>
2018-08-05 11:10:14 +0800

    第三十次提交

--- hello/Main.java
+++ hello/Main.java
@@ -3,18 +3,7 @@ package hello;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< Upstream, based on 718fa937cfa5151127a6e5f68459aa47888d2cbd
-<<<<<<< HEAD
-		System.out.println("Hello World!");//第二十六次提交
-=======
-		System.out.println("Hello World!");//第二十四次提交 第七次合并代码
-=======
-		System.out.println("Hello World!");//第二十七次提交
->>>>>>> 0b4e3a6 第二十七次提交
-=======
-		System.out.println("Hello World!");//第二十九次提交
->>>>>>> branch 'master' of https://github.com/iceexcel/gittest.git
+		System.out.println("Hello World!");//第三十次提交
 	}
 
 }
