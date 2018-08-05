de9ca0d9cca48b5836abdfa6cdb9997a627a6864
Administrator <Administrator@A038>
2018-08-05 13:19:13 +0800

    主干第四十次提交

--- hello/Main.java
+++ hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");//主干第三十九次提交
+		System.out.println("Hello World!");//主干第四十次提交
 	}
 
 }
