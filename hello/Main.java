cced1bf6205690358679220554b1ce41365e4ce1
Administrator <Administrator@A038>
2018-08-05 11:24:05 +0800

    第十三次提交，删除无用的代码dev003

--- hello/Main.java
+++ hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");//第十三次提交，删除无用的代码dev
+		System.out.println("Hello World!");//第十三次提交，删除无用的代码dev002
 	}
 
 }
