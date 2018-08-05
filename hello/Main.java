9e82454c12316e1c3ea07ff1ed10cd52281d4ba6
Administrator <Administrator@A038>
2018-08-05 11:48:34 +0800

    dev005

--- hello/Main.java
+++ hello/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
 <<<<<<< HEAD
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello World!");//第三十一次提交,合并主干
 >>>>>>> refs/heads/master
 =======
@@ -12,7 +11,7 @@ public class Main {
 =======
 	public static void main(String[] args) {
 <<<<<<< HEAD
-		System.out.println("Hello World!");//第十三次提交，删除无用的代码dev002
+		System.out.println("Hello World!");//第十三次提交，删除无用的代码dev005
 >>>>>>> refs/remotes/origin/develop
 >>>>>>> refs/heads/master
 =======
