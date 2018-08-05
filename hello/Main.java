bfc484a3bd78a328e5ee2f7347b228b939a452ee
Administrator <Administrator@A038>
2018-08-05 11:41:20 +0800

    合并主干002

--- hello/Main.java
+++ hello/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.println("Hello World!");//第十三次提交，删除无用的代码dev002
 =======
 	public static void main(String[] args) {
-		System.out.println("Hello World!");//第三十一次提交
+		System.out.println("Hello World!");//第三十一次提交,合并主干
 >>>>>>> refs/heads/master
 	}
 
