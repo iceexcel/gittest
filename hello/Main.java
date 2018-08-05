4a1ee9b91a67f320d3f925273b63d43bdda4a355
Administrator <Administrator@A038>
2018-08-05 11:02:27 +0800

    冲突了

--- hello/Main.java
+++ hello/Main.java
@@ -3,7 +3,11 @@ package hello;
 public class Main {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("Hello World!");//第二十六次提交
+=======
+		System.out.println("Hello World!");//第二十四次提交
+>>>>>>> 915e2f6 第二十五次提交
 	}
 
 }
