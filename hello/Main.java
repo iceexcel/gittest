8bcd9cbb0208202aeeebbeef59a160bd8e980ff8
Administrator <Administrator@A038>
2018-08-05 11:03:04 +0800

    第一次合并代码

--- hello/Main.java
+++ hello/Main.java
@@ -6,8 +6,7 @@ public class Main {
 <<<<<<< HEAD
 		System.out.println("Hello World!");//第二十六次提交
 =======
-		System.out.println("Hello World!");//第二十四次提交
->>>>>>> 915e2f6 第二十五次提交
+		System.out.println("Hello World!");//第二十四次提交 第一次合并代码
 	}
 
 }
