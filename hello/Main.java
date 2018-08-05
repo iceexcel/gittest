718fa937cfa5151127a6e5f68459aa47888d2cbd
Administrator <Administrator@A038>
2018-08-05 10:48:46 +0800

    第二十六次提交

--- hello/Main.java
+++ hello/Main.java
@@ -3,7 +3,7 @@ package hello;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");//第二十五次提交
+		System.out.println("Hello World!");//第二十六次提交
 	}
 
 }
