81bf473b6a77fa7cd3d5cadef8ed6bcf4f8cf7d7
Administrator <Administrator@A038>
2018-08-05 12:22:21 +0800

    主干第三十七次提交

--- hello/Main.java
+++ hello/Main.java
@@ -3,23 +3,7 @@ package hello;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello World!");//主干第三十六次提交
-=======
-		System.out.println("Hello World!");//第三十一次提交,合并主干
->>>>>>> refs/heads/master
-=======
-		System.out.println("Hello World!");//第三十一次提交
-=======
-	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello World!");//第十三次提交，删除无用的代码dev005
->>>>>>> refs/remotes/origin/develop
->>>>>>> refs/heads/master
-=======
-		System.out.println("Hello World!");//第十三次提交，删除无用的代码dev003
->>>>>>> branch 'develop' of https://github.com/iceexcel/gittest.git
->>>>>>> refs/remotes/origin/develop
+		System.out.println("Hello World!");//主干第三十七次提交
 	}
 
 }
