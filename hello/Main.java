e77fe0731bd58a9f71213017737ddd894ef3915b
Administrator <Administrator@A038>
2018-08-05 11:03:25 +0800

    第二次合并代码

--- hello/Main.java
+++ hello/Main.java
@@ -6,7 +6,7 @@ public class Main {
 <<<<<<< HEAD
 		System.out.println("Hello World!");//第二十六次提交
 =======
-		System.out.println("Hello World!");//第二十四次提交 第一次合并代码
+		System.out.println("Hello World!");//第二十四次提交 第二次合并代码
 	}
 
 }
