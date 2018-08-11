5eafe894b0d83a26e56ad8319f3def162444578c
Administrator <Administrator@60.207.84.28>
2018-08-11 13:39:34 +0800

    Hello World!

--- hello/Main.java
+++ hello/Main.java
@@ -1,24 +1,10 @@
 package hello;
 
-<<<<<<< HEAD
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-		System.out.println("Hello World!");//主干第四十一次提交
-=======
-		System.out.println("Hello World!");//主干第四十次提交local001
->>>>>>> refs/remotes/origin/develop
-=======
-		System.out.println("Hello World!");//第三十一次提交
->>>>>>> refs/remotes/origin/hello-branch
-=======
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");//第一个分支第二次提交ddd
->>>>>>> refs/remotes/origin/bbb
+		System.out.println("Hello World!");
 	}
 
 }
+
